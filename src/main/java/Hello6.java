50c7e346d378db530b5424b46cae99e3d66e22a4
data8320-26 <wj020315@naver.com>
2024-03-12 09:51:27 +0900

    글자 바꾸기

--- src/main/java/Hello6.java
+++ src/main/java/Hello6.java
@@ -3,6 +3,7 @@ public class Hello6 {
     public static void main (string[] args) {
         System.out.println("Hello World");
         System.out.println("Hi");
+        System.out.println("Change the word");
 
     }
 }
